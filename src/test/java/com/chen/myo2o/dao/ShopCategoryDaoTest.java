9ada9165a195756025ced318c26f154ea4428165
chenyaowu <chenyaowu15@gmail.com>
2019-03-24 23:58:07 +0800

    shopCategory

---
+++ src/test/java/com/chen/myo2o/dao/ShopCategoryDaoTest.java
@@ -0,0 +1,26 @@
+package com.chen.myo2o.dao;
+
+import com.chen.myo2o.BaseTest;
+import com.chen.myo2o.entity.ShopCategory;
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ShopCategoryDaoTest extends BaseTest {
+    @Autowired
+    private ShopCategoryDao shopCategoryDao;
+
+    @Test
+    public void testQueryShopCategory(){
+        List<ShopCategory> shopCategoryList = shopCategoryDao.queryShopCategory(null);
+        assertEquals(1,shopCategoryList.size());
+        ShopCategory testCategory = new ShopCategory();
+        ShopCategory parentCategory = new ShopCategory();
+        parentCategory.setShopCategoryId(1l);
+        testCategory.setParent(parentCategory);
+        shopCategoryList = shopCategoryDao.queryShopCategory(testCategory);
+        assertEquals(1,shopCategoryList.size());
+    }
+}
